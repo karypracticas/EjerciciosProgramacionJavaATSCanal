b32b4653ffda2c08ae78a56da4ce81bb6fdf7cb7
Kary Rguez <practicasgithub@gmail.com>
2020-10-06 17:17:22 -0500

    Ejercicio 55.Definicion de Matriz

---
+++ src/main/java/com/mycompany/ejerciciosordenados/Matriz.java
@@ -0,0 +1,48 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.ejerciciosordenados;
+
+import java.util.Scanner;
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author kary
+ */
+public class Matriz {
+    
+     public static void main(String[] args) {
+        
+         //55. Definición de matriz
+    Scanner entrada =  new Scanner(System.in);
+    int matriz[][], nFilas,nColumnas;
+    
+    nFilas = Integer.parseInt(JOptionPane.showInputDialog("Número de filas:"));
+    nColumnas = Integer.parseInt(JOptionPane.showInputDialog("Número de Columnas:"));
+    matriz = new int[nFilas][nColumnas];
+    
+    //Llenar arreglo
+    //TODO: Verificar este ciclo
+        System.out.println("Digite Matriz: ");
+    for(int i=0; i<nFilas;i++){
+        for(int j=0; j<nColumnas;j++){
+            System.out.print("Matriz ["+i+"]["+j+"]: ");
+            matriz[i][j] = entrada.nextInt();
+        }
+    }
+    
+    
+    //Imprimir matriz
+      System.out.print("Imprimir Matriz");
+    for(int i=0; i<nFilas;i++){ //Filas
+        for(int j=0; j<nColumnas;j++){ //Columnas
+            System.out.println(matriz[i][j]);
+        }
+        System.out.println("");
+    } 
+  } 
+    
+}
