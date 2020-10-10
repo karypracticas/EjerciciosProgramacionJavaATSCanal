5bd03d8471316b0c7691ab14025c192276b17950
practicaskary2 <practicasgithub2@gmail.com>
2020-10-10 11:42:24 -0500

    Ejercicio 72. Completo

--- src/main/java/POOVehiculo/Main.java
+++ src/main/java/POOVehiculo/Main.java
@@ -9,16 +9,30 @@ import java.util.Scanner;
 
 public class Main {
     
-    //Prueba de hacer push desde asus
+    public static int indiceAutoMBarato(Vehiculo autos[]){
+        float precio;
+        int indice = 0;
+        
+        precio = autos[0].getPrecio();
+        for(int i=1;i<autos.length;i++){
+            if(autos[i].getPrecio()<precio){
+                precio = autos[i].getPrecio();
+                indice = i;
+            }
+        }
+        
+        return indice;
+    }
+    
     
     public static void main(String[] args) {
         
        Scanner entrada = new Scanner(System.in);
        String marca,modelo;
        float precio;
-       int numeroVehiculos;
+       int numeroVehiculos,indiceBarato;
        
-       System.out.println("Digite la cantidad de vehículos");
+       System.out.print("Digite la cantidad de vehículos:");
        numeroVehiculos = entrada.nextInt();
        
        //Creamos los objetos para los autos
@@ -26,16 +40,21 @@ public class Main {
         
        //Llenar el arreglo con la información de los autos
        for(int i=0;i<autos.length;i++){
+           entrada.nextLine();
            System.out.println("Ingrese los datos del auto " + (i+1) + ":");
-           System.out.println("Ingrese la marca: ");
+           System.out.print("Ingrese la marca: ");
            marca = entrada.nextLine();
-           System.out.println("Ingrese el modelo: ");
+           System.out.print("Ingrese el modelo: ");
            modelo = entrada.nextLine();
-           System.out.println("Ingrese el precio:");
+           System.out.print("Ingrese el precio:");
            precio = entrada.nextFloat();
            
            autos[i] = new Vehiculo(marca,modelo,precio);
        }
+       
+       indiceBarato = indiceAutoMBarato(autos);
+        System.out.println("El auto más barato es: ");
+        System.out.println(autos[indiceBarato].mostrarDatos());
         
     }
     
