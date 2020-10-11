a2013f7beb0f7d78f906790d0ee9111a0e1821bb
practicaskary2 <practicasgithub2@gmail.com>
2020-10-11 18:18:14 -0500

    Ejercicio 82. Ejemplos excepciones

---
+++ src/main/java/EjemplosExcepciones/Excepciones.java
@@ -0,0 +1,22 @@
+package EjemplosExcepciones;
+
+import java.util.Scanner;
+
+public class Excepciones {
+    public static void main(String[] args) {
+       //Ejemplo de una excepción al dividir un entero entre 0
+       /* int num1 = 5;
+        int num2 = 0;
+        int resultado;
+        
+        resultado = num1 / num2;
+        System.out.println("Resultado");*/
+       
+       //Ejemplo de ingresar un tipo de dato incorrecto al solicitado
+        /*Scanner entrada = new Scanner(System.in);
+        System.out.println("Introduce un número entero: ");
+        int numero = entrada.nextInt();
+        System.out.println(numero);*/
+       
+    }
+}
